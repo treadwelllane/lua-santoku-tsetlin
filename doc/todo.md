7448b3038f91f7e83bcd3f07347dce0ed120253f
Matthew Brooks <mb964033@gmail.com>
2024-02-26 19:16:17 +0000

    initial

---
+++ doc/todo.md
@@ -0,0 +1,11 @@
+# Now
+
+- Allow batch updates passed as a row-major bitmatrix
+
+- Tsetlin Machine advancements
+  - SIMD
+  - Drop clause (increases accuracy and learning speed)
+  - Indexed (improves learning and classification speed)
+  - Weighted (reduces memory footprint)
+  - Multi-granular (eliminates hyper-parameter S(ensitivity))
+  - Coalesced (reduces memory footprint for multi-output configurations)
---
+++ make.lua
@@ -0,0 +1,29 @@
+local env = {
+
+  name = "santoku-tsetlin",
+  version = "0.0.1-1",
+  variable_prefix = "TK_TSETLIN",
+  public = true,
+
+  dependencies = {
+    "lua == 5.1",
+    "santoku >= 0.0.196-1",
+  },
+
+  test = {
+    dependencies = {
+      "luacov == 0.15.0-1",
+      "santoku-bitmap >= 0.0.2-1",
+    }
+  },
+
+}
+
+env.homepage = "https://github.com/treadwelllane/lua-" .. env.name
+env.tarball = env.name .. "-" .. env.version .. ".tar.gz"
+env.download = env.homepage .. "/releases/download/" .. env.version .. "/" .. env.tarball
+
+return {
+  type = "lib",
+  env = env,
+}
