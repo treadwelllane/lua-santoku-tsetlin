193bc9b06c2362c1363c94640a42ec680ed74cbb
Matthew Brooks <mb964033@gmail.com>
2024-05-19 18:27:41 +0000

    update test flags

--- make.lua
+++ make.lua
@@ -1,7 +1,7 @@
 local env = {
 
   name = "santoku-tsetlin",
-  version = "0.0.34-1",
+  version = "0.0.35-1",
   variable_prefix = "TK_TSETLIN",
   public = true,
 
@@ -14,8 +14,8 @@ local env = {
   },
 
   test = {
-    cflags = { "-fopt-info-vec=optimize.txt", "-fopt-info-vec-missed=optimize.txt" },
-    ldflags = { "-fopt-info-vec=optimize.txt", "-fopt-info-vec-missed=optimize.txt" },
+    cflags = { "-fopt-info-vec=optimize.txt", "-fopt-info-vec-missed=optimize.txt", "-g3" },
+    ldflags = { "-fopt-info-vec=optimize.txt", "-fopt-info-vec-missed=optimize.txt", "-g3" },
     dependencies = {
       "luacov >= 0.15.0-1",
       "santoku-bitmap >= 0.0.9-1",
