960ceec970aa6208ec4598266b469eaab4365031
Matthew Brooks <mb964033@gmail.com>
2025-03-30 01:01:40 +0000

    update dep

--- make.lua
+++ make.lua
@@ -18,7 +18,7 @@ local env = {
     ldflags = { "-fopt-info-vec=optimize.txt", "-fopt-info-vec-missed=optimize.txt", "-g3" },
     dependencies = {
       "luacov >= 0.15.0-1",
-      "santoku-bitmap >= 0.0.30-1",
+      "santoku-bitmap >= 0.0.31-1",
       "santoku-matrix >= 0.0.13-1",
       "santoku-fs >= 0.0.34-1",
     }
