cc671fc99e89250e6fab0225442b5dbb3a40c2c6
Matthew Brooks <mb964033@gmail.com>
2025-07-28 10:34:34 -0400

    remove flto=thin

--- make.lua
+++ make.lua
@@ -1,6 +1,6 @@
 local arr = require("santoku.array")
 local fs = require("santoku.fs")
 local base = fs.runfile("make.common.lua")
-base.env.cflags = arr.extend({ "-O3", "-march=native", "-flto=thin", "-fdata-sections", "-ffunction-sections" }, base.env.cflags)
-base.env.ldflags = arr.extend({ "-O3", "-march=native", "-flto=thin", "-Wl,-plugin-opt=O3", "-Wl,--gc-sections" }, base.env.ldflags)
+base.env.cflags = arr.extend({ "-O3", "-march=native", "-fdata-sections", "-ffunction-sections" }, base.env.cflags)
+base.env.ldflags = arr.extend({ "-O3", "-march=native", "-Wl,-plugin-opt=O3", "-Wl,--gc-sections" }, base.env.ldflags)
 return base
