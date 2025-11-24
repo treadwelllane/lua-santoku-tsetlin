0cfffdca78b6f6a07750d1c75f170d75907b77d5
Matthew Brooks <mb964033@gmail.com>
2025-11-23 20:11:23 -0500

    update

--- make.common.lua
+++ make.common.lua
@@ -1,6 +1,6 @@
 local env = {
   name = "santoku-tsetlin",
-  version = "0.0.245-1",
+  version = "0.0.246-1",
   variable_prefix = "TK_TSETLIN",
   license = "MIT",
   public = true,
@@ -52,7 +52,7 @@ local env = {
   dependencies = {
     "lua >= 5.1",
     "santoku >= 0.0.294-1",
-    "santoku-matrix >= 0.0.189-1",
+    "santoku-matrix >= 0.0.190-1",
   },
   test = {
     dependencies = {
